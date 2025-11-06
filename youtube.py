00635996811c1d78661b454e0092ed4bd664af66
Rodriguez JHORDAN <rodriguez.jhordan@alten.it>
2025-11-06 11:32:27 +0100

    youtube script Firefox version added

---
+++ youtube.py
@@ -0,0 +1,427 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 22:17:38 2024
+
+@author: 
+
+https://brightdata.com/blog/how-tos/how-to-scrape-youtube-in-python
+"""
+
+# pip install multiprocess
+# pip install selenium
+# pip install webdriver_manager
+import multiprocessing
+from selenium import webdriver
+
+
+# A Service class is responsible
+# for the starting and stopping of chromedriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
+
+# An explicit wait is a code you define
+# to wait for a certain condition to occur
+# before proceeding further in the code
+from selenium.webdriver.support import expected_conditions as EC
+
+# Selenium Python binding provides some convenience methods
+# so you don’t have to code an expected_condition class yourself
+# An expectation for checking an element is visible
+# and enabled such that you can click it.
+from selenium.common.exceptions import NoSuchElementException
+
+# Thrown when element could not be found.
+import traceback
+import time
+import os
+
+# homepage = "https://www.youtube.com/@AthanasiouApostolos/videos"
+#consent_button_xpath_homepage = "//button[@aria-label='Accept all']"
+consent_button_xpath_homepage ="//input[@type='submit' and @value='I agree']"
+video_links_class_name = "yt-simple-endpoint.ytd-thumbnail"
+consent_button_xpath = "//button[@aria-label='Accept the use of cookies and other data for the purposes described']"
+#consent_button_xpath = "//button[@aria-label='Accept all']"
+# ads_button_selector = "button.ytp-ad-skip-button, button.ytp-skip-ad-button"
+# ads_button_selectors = ["button.ytp-ad-skip-button", "button.ytp-skip-ad-button"]
+ads_button_selectors = [".ytp-skip-ad button"]
+mute_button_class = "ytp-mute-button"
+replay_xpath = '//*[@title="Replay"]'
+# // -> Selects nodes in the document from the current node
+# that match the selection no matter where they are
+# * -> Matches any element node
+
+start_time = int(time.time())
+
+def play():
+    #homepage = input("Please provide channel url: ")
+    homepage = "https://www.youtube.com/watch?v=PdzOkN9_F9A"
+    print(f"RUN: {start_time} | {homepage}")
+    try:
+        srv = webdriver.FirefoxService(os.path.join("driver", "geckodriver"))
+        opt = webdriver.FirefoxOptions()
+        opt.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36')
+        #opt.add_argument('--start-maximized')
+        #opt.add_argument('--no-sandbox')
+        #opt.add_argument('--user-data-dir /home/admin/Software/youtube-selenium/userData')
+        #opt.add_argument('--profile-directory=Profile1')
+        opt.add_argument('--disable-gpu')
+        opt.add_argument('--disable-accelerated-2d-canvas')
+        opt.add_argument('--disable-blink-features=AutomationControlled')
+        opt.add_argument('--allow-running-insecure-content')
+        opt.add_experimental_option('excludeSwitches', ['enableAutomation'])
+        opt.add_experimental_option('useAutomationExtension', False)
+
+        exc = '/home/admin/Software/youtube-selenium/chromedriver/chromedriver'
+        #opt.binary_location = '/home/admin/Software/youtube-selenium/chromedriver/chromedriver'
+        driver = webdriver.Chrome(service=srv, options=opt)
+        time.sleep(3)
+        driver.get(homepage)
+        #consent_button = WebDriverWait(driver, 30).until(
+        #    EC.presence_of_element_located((By.XPATH, consent_button_xpath_homepage)) #EC.element_to_be_clickable((By.XPATH, consent_button_xpath_homepage))
+        #)
+        
+        #consent_button = driver.find_element(by=By.CLASS_NAME, value="yt-spec-button-shape-next yt-spec-button-shape-next--filled yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m")
+        #consent_button.click()
+        
+        try:
+            # wait up to 15 seconds for the consent dialog to show up
+            consent_overlay = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.ID, 'dialog'))
+            )
+            time.sleep(2)
+
+            # select the consent option buttons
+            consent_buttons = consent_overlay.find_elements(By.CSS_SELECTOR, '.eom-buttons button.yt-spec-button-shape-next')
+            if len(consent_buttons) > 1:
+                # retrieve and click the 'Accept all' button
+                accept_all_button = consent_buttons[1]
+                accept_all_button.click()
+                print(f'RUN: {start_time} | Accepted cookes')
+        except Exception:
+            print(f'RUN: {start_time} | Cookie modal missing')
+            
+        # wait for YouTube to load the page data
+        WebDriverWait(driver, 15).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, 'h1.ytd-watch-metadata'))
+            )
+        time.sleep(1)
+            
+        print(f"RUN: {start_time} | ts {int(time.time())} Video should start shortly")
+        #driver.execute_script('document.getElementsByClassName("ytp-contextmenu")[0].style.display = "block";');
+        #driver.execute_script('document.getElementsByClassName("ytp-menuitem")[6].click();');
+        
+        #print("Stats enabled") 
+        
+        movie_player = driver.find_element(by=By.ID, value='movie_player')
+        hover = ActionChains(driver).move_to_element(movie_player)
+        hover.perform()
+        ActionChains(driver).context_click(movie_player).perform()
+        """
+        options = driver.find_elements(by=By.CLASS_NAME, value='ytp-menuitem')
+        for option in options:
+            option_child = option.find_element(by=By.CLASS_NAME, value='ytp-menuitem-label')
+            if ' nerd' in option_child.text:
+                option_child.click()
+                print(f"RUN: {start_time} | Enabled stats collection.")
+        """
+        
+        time.sleep(2)
+
+        DIV_TO_KEY = {
+            # '0': "script_time",
+            '1': "Video ID / sCPN",
+            '2': "Viewport / Frames",
+            '3': "Current / Optimal Res",
+            '4': "Volume / Normalized",
+            '5': "Codecs",
+            '6': "Color",
+            '9': "Connection Speed", 
+            '10': "Network Activity", 
+            '11': "Buffer Health",
+            '12': "Live Latency",
+            '15': "Mystery Text",
+
+            # '16': "time",
+            # '17': "start_time",
+        }
+        keys = [
+            "Video ID / sCPN",
+            "Viewport / Frames",
+            "Current / Optimal Res",
+            "Volume / Normalized",
+            "Codecs",
+            "Color",
+            "Connection Speed", 
+            "Network Activity", 
+            "Buffer Health",
+            "Live Latency",
+            "Mystery Text"
+        ]
+
+        headers = ['script_time', 'start_time', 'Video ID', 'Frames', 'Current Res', 'Connection Speed', 'Network Activity', 'Buffer Health', 'time', 'i']
+        video_id = "PdzOkN9_F9A"
+        last_not_found_div_id = ''
+
+        file_path = "youtube_stats.txt"
+        first_line = None
+        if not os.path.isfile(file_path):
+            first_line = f"{';'.join(headers)}\n"
+            # print(f"RUN: {start_time} | The stats file does not exist.")
+        
+        with open(file_path, 'a+') as f:
+            if first_line is not None:
+                f.write(first_line)
+
+            j = 0
+            i = 0
+            factor = float(0)
+            enable_skipping = True
+            last_video_id = video_id
+            last_ad_ts = 0.0
+            last_ts = 0.0
+            last_buffer = 0.0
+            last_res = ''
+            
+            while j < 10000:
+                try:
+                    hover.perform()
+                    stat_dict = {}
+                    not_found_div_id = []
+
+                    try:
+                        elements = driver.find_element(by=By.CSS_SELECTOR, value=f".html5-video-info-panel-content").text
+                        elements = elements.split("\n")
+                        cnt = 0
+                        for elem in elements:
+                            for key in keys[cnt:]:
+                                if elem.startswith(key):
+                                    res = elem.split(key)[-1].strip()
+                                    break
+                            else:
+                                continue
+
+                            res = elem.split(key)[-1].strip()
+                            if ' / ' in res:
+                                res = res.split(' / ')
+
+                            if 'Video ID' in key:
+                                stat_dict['Video ID'] = res[0]
+                            elif 'Viewport' in key:
+                                stat_dict['Viewport'] = res[0]
+                                stat_dict['Frames'] = res[1]
+                            elif 'Optimal Res' in key:
+                                stat_dict['Current Res'] = res[0]
+                                stat_dict['Optimal Res'] = res[1]
+                            elif 'Buffer Health' in key:
+                                stat_dict['Buffer Health'] = res.rstrip(' s')
+                            else:
+                                stat_dict[key] = res
+    
+                            cnt += 1
+                    except NoSuchElementException: pass
+                    except Exception:
+                        print(traceback.format_exc())
+
+                    if len(keys) - len(stat_dict) > 0:
+                        not_found_div_id = set(keys)-set(stat_dict)
+
+                    if ','.join(list(not_found_div_id)) != last_not_found_div_id:
+                        # print(f'RUN: {start_time} | Error not found div_id {not_found_div_id}')
+                        last_not_found_div_id = ','.join(list(not_found_div_id)) 
+
+                    if "Video ID" in not_found_div_id or len(stat_dict) == 0:
+                        print('Video ID not found')
+
+                        try:
+                            options = driver.find_elements(by=By.CLASS_NAME, value='ytp-menuitem')
+                            for option in options:
+                                option_child = option.find_element(by=By.CLASS_NAME, value='ytp-menuitem-label')
+                                if ' nerd' in option_child.text:
+                                    option_child.click()
+                                    print(f"RUN: {start_time} | Enabled stats collection.")
+                                    break
+
+                            time.sleep(2)
+                        except:
+                            pass
+
+                        continue
+
+                    stat_dict['Network Activity'] = stat_dict['Network Activity'].rstrip(' KB')
+                    stat_dict['start_time'] = str(start_time)
+                    stat_dict['script_time'] = str(int(time.time()))
+
+                    ts = 0.0
+                    try:
+                        ts = float(stat_dict["Mystery Text"].split(' b:')[0].split(' t:')[-1])
+                    except Exception as e:
+                        print(stat_dict["Mystery Text"], e)
+                        print(f"RUN: {start_time} | {stat_dict['Video ID']} Could not recover time from Mystery Text")
+                        ts = last_ts + 0.5  # try sth
+
+                    if video_id not in stat_dict['Video ID']:                      
+                        if not enable_skipping:
+                            print(f"RUN: {start_time} | {stat_dict['Video ID']} | ts {int(time.time())} | vid time: {ts} (last: {last_ts}) Video ID Changed, Ending.")
+                            f.flush()
+                            break
+                        click_skip_adds(driver)
+
+                    if stat_dict['Video ID'] != last_video_id:
+                        # switch pubblicità/video e viceversa
+                        last_video_id = stat_dict['Video ID']
+                        last_ad_ts = 0.0
+
+                    if video_id not in stat_dict['Video ID'] and ts < last_ts:
+                        ts = last_ts + ts  # adv restarts time from 0 (even multiple times)
+
+                    if '@' not in stat_dict['Current Res'] or '0x0@' in stat_dict['Current Res']:
+                        stat_dict['Current Res'] = last_res  # ipotizziamo che la res sia uguale all'ultima
+
+                    if video_id in stat_dict['Video ID']:
+                        last_res = stat_dict['Current Res']
+
+                        try:
+                            cur_buffer = float(stat_dict['Buffer Health'])
+                            # prevent foolish drops in buffer health
+                            if cur_buffer < last_buffer-10.0 and last_buffer > 10.0 and ts - last_ts < 5.0:
+                                stat_dict['Buffer Health'] = str(last_buffer)
+                            last_buffer = cur_buffer
+                        except Exception:
+                            print(f"RUN: {start_time} | {stat_dict['Video ID']} | ts {stat_dict['script_time']} | Vid time: {ts} | Buffer invalid: {stat_dict['Buffer Health']} | cur res: {stat_dict['Current Res']}")
+                            continue
+
+                        if ts < last_ts and last_ts > 0.0:
+                            print(f"RUN: {start_time} | {stat_dict['Video ID']} | ts {stat_dict['script_time']} | Vid time invalid: {ts} (last: {last_ts}) | cur res: {stat_dict['Current Res']}")
+                            continue
+
+                        if ts > 1960:
+                            enable_skipping = False
+
+                        if ts - last_ts > 1.2 and factor < 0.9:
+                            factor = factor+0.1
+                        elif ts - last_ts < 0.8 and factor > 0.3:
+                            factor = factor-0.1
+
+                        last_ts = ts
+                    else:
+                        if ts < last_ad_ts and last_ad_ts > 0.0:
+                            print(f"RUN: {start_time} | {stat_dict['Video ID']} | ts {stat_dict['script_time']} | Vid time invalid: {ts} (last: {last_ad_ts}) | cur res: {stat_dict['Current Res']}")
+                            continue
+                        last_ad_ts = ts
+
+                    stat_dict['time'] = str(ts).replace('.', ',')
+                    stat_dict['Buffer Health'] = stat_dict['Buffer Health'].replace('.', ',')
+
+                    s = [stat_dict[s] for s in headers[:-1]]
+                    f.write(f"{';'.join(s)};{i}\n")
+
+                    if video_id in stat_dict['Video ID']:
+                        if i % 10 == 0:
+                            f.flush()
+                            try:
+                                res = stat_dict['Current Res']
+                                if '1920x' not in res:
+                                    change_resolution(driver)
+                            except Exception as e:
+                                print(f"RUN: {start_time} | Exception while trying to change resolution {e}")
+
+                        if i < 10 or i % 30 == 0:
+                            print(f"RUN: {start_time} | {stat_dict['Video ID']} | ts {int(time.time())} | vid time: {ts} | cur res: {stat_dict['Current Res']}")
+
+                        i = i+1
+                    else:
+                        print(f"RUN: {start_time} | {stat_dict['Video ID']} | ts {int(time.time())} | vid time: {ts} | cur res: {stat_dict['Current Res']}")
+
+                    sleep_time = 1.0
+                    if 1 - factor > 0.3:
+                        sleep_time = sleep_time-factor
+
+                    time.sleep(sleep_time)
+                except Exception as e:
+                    print(f"RUN: {start_time} | ts {int(time.time())} Error {traceback.format_exc()}")
+
+        print(f"RUN: {start_time} | Ending")
+    finally:
+        try:
+            driver.close()
+        except:
+            pass
+
+        try:
+            driver.quit()
+        except:
+            pass
+    return True
+
+def change_resolution(driver):
+    """ to be tested """
+    resolution = "1080p"
+    resolution2 = "720p"
+    print(f"RUN: {start_time} | Selecting resolution {resolution}")
+    time.sleep(0.2)
+    sb = driver.find_element(by=By.CSS_SELECTOR, value='.ytp-button.ytp-settings-button')
+    sb.click()
+    time.sleep(0.3)
+    try:
+        elem = driver.find_element(by=By.CSS_SELECTOR, value='div.ytp-menuitem[role="menuitem"] > div.ytp-menuitem-content span')
+        elem.click()
+    except:
+        try:
+            elem = driver.find_element(by=By.CSS_SELECTOR, value='div.ytp-menuitem:nth-child(5) > div:nth-child(1)')
+            elem.click()
+        except:
+            elem = driver.find_element(by=By.CSS_SELECTOR, value='div.ytp-menuitem:nth-child(4) > div:nth-child(1)')
+            elem.click()
+
+    time.sleep(2)
+    res = driver.find_elements(by=By.CLASS_NAME, value="ytp-menuitem-label")
+    for item in res:
+        # print(item.text)
+        if resolution in item.text:
+            item.click()
+            print(f"RUN: {start_time} | Resolution selected", resolution)
+            break
+    else:
+        for item in res:
+            if resolution2 in item.text:
+                item.click()
+                print(f"RUN: {start_time} | Resolution selected", resolution2)
+                break
+
+        print(f'RUN: {start_time} | Resolution {resolution} not available yet')
+
+
+def click_skip_adds(driver):
+    for selector in ads_button_selectors:
+        try:
+            skip_adds_button = driver.find_element(By.CSS_SELECTOR, selector)
+            if not skip_adds_button.is_displayed():
+                time.sleep(3)
+                driver.execute_script("arguments[0].style.display = 'block';", skip_adds_button)
+            skip_adds_button.click()
+            print(f'RUN: {start_time} | ts {int(time.time())} Skipped advertisement')
+            break
+        except NoSuchElementException as e:
+            pass
+        except Exception:
+            pass
+            # Most common "Element not interactable"
+            # print('Exception skipping', selector, traceback.format_exc())
+    # else:
+    #    try:
+    #        print("Could not skip adv, button source:", driver.find_element(By.CSS_SELECTOR, '.ytp-skip-ad').get_attribute("outerHTML"))
+    #    except Exception:
+    #        print(traceback.format_exc())
+
+if __name__ == "__main__":
+    retry = 0
+    while retry < 5:
+        try:
+            play()
+            break
+        except Exception as e:
+            print(f"RUN: {start_time} | ts {int(time.time())} Exception outside video playing, retry {retry} {traceback.format_exc()}")
+
+        retry+=1
+        time.sleep(5)
